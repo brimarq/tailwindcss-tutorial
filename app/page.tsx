081478deb9b97b3b7f0dcb0f83c097da9a0e8b32
Brian Marquis <rbmarquis.dev@gmail.com>
2023-02-18 16:37:28 -0600

    use square-bracket notation for arbitrary values

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,16 @@ export default function Home() {
   return (
     <div>
       <h1 className="border-4 border-green-600">Hello world!</h1>
-      <h1 className="text-vuejs-100">Hello world!</h1>
+      <h1 className="text-vuejs-100">Using </h1>
       <p className="text-xs sm:text-base md:text-lg lg:text-3xl">
-        Hello long sentence
+        Using screen classes for media queries
       </p>
-      <p className="mb-1">Hello long sentence</p>
+      <p className="mb-1">Using custom spacing</p>
       <p>Another long sentence</p>
+      <p className="bg-[#bada55]">
+        Using TW square-bracket notation to generate a class on the fly with any
+        arbitrary value.
+      </p>
     </div>
   );
 }
