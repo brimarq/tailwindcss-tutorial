836eb5edc00c6253621a6a6b12d519f90f645eb1
Brian Marquis <rbmarquis.dev@gmail.com>
2023-02-18 16:07:47 -0600

    demo custom media queries and spacing

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,11 @@ export default function Home() {
     <div>
       <h1 className="border-4 border-green-600">Hello world!</h1>
       <h1 className="text-vuejs-100">Hello world!</h1>
+      <p className="text-xs sm:text-base md:text-lg lg:text-3xl">
+        Hello long sentence
+      </p>
+      <p className="mb-1">Hello long sentence</p>
+      <p>Another long sentence</p>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,20 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    spacing: {
+      1: "100px",
+      2: "12px",
+      3: "16px",
+      4: "24px",
+      5: "32px",
+      6: "48px",
+    },
     extend: {
       colors: {
         vuejs: {
